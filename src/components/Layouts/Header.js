2e91b69a95a01532c2c76f2a3727bce319cdc856
Rahul7044 <rahulkumar581999@gmail.com>
2023-04-21 01:28:25 +0530

    contactus-page via router

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,12 +1,11 @@
 import React, { useContext } from "react";
 import { FaCartPlus } from "react-icons/fa";
-import { Badge, Container, Dropdown,Nav, Navbar } from "react-bootstrap";
+import { Badge, Container, Dropdown, Navbar } from "react-bootstrap";
 import Cart from "../cart/Cart";
 import CartContext from "../../store/CartContext";
 import { NavLink } from "react-router-dom";
 function Header() {
   const ctx = useContext(CartContext);
-
   let quantity = 0;
   ctx.items.forEach((item) => {
     quantity += Number(item.quantity);
@@ -21,20 +20,20 @@ function Header() {
           <NavLink to="Home">Home</NavLink>
           <NavLink to="/">Store</NavLink>
           <NavLink to="About">About</NavLink>
-          <Nav>
-            <Dropdown align="end">
-              <Dropdown.Toggle variant="dark">
-                <FaCartPlus />
+          <NavLink to="contact">Contact</NavLink>
 
-                <Badge bg="dark" text="info">
-                  {quantity}
-                </Badge>
-              </Dropdown.Toggle>
-              <Dropdown.Menu style={{ minWidth: 370 }}>
-                <Cart></Cart>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Nav>
+          <Dropdown align="end">
+            <Dropdown.Toggle variant="dark">
+              <FaCartPlus />
+              <Badge bg="dark" text="info">
+                {quantity}
+              </Badge>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu style={{ minWidth: 370 }}>
+              <Cart></Cart>
+            </Dropdown.Menu>
+          </Dropdown>
         </Container>
       </Navbar>
     </div>
